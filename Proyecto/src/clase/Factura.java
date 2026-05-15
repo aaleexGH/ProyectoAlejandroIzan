cac55f5deede9b083ffc3b3a84e45921a18056bd
1DAM <1DAM@A11-PC18>
2026-05-15 09:28:53 +0200

    DAO

--- Proyecto/src/clase/Factura.java
+++ Proyecto/src/clase/Factura.java
@@ -1,8 +1,12 @@
 package clase;
 
+import java.sql.Date;
+import java.time.LocalDate;
+
 public class Factura {
 	
 	private int id;
+	private LocalDate fecha;
 	private int id_cliente;
 	private int id_empleado;
 	private double subtotal;
@@ -13,58 +17,100 @@ public class Factura {
 	public Factura() {
 		super();
 	}
-	public Factura(double subtotal, double iva, double total) {
-		this(0, 0, 0, subtotal, iva, total);
+
+
+	public Factura(LocalDate fecha, int id_cliente, int id_empleado, double subtotal, double iva, double total) {
+		this(0, fecha, id_cliente, id_empleado, subtotal, iva, total);
 	}
-	public Factura(int id, int id_cliente, int id_empleado, double subtotal, double iva, double total) {
+
+
+	public Factura(int id, LocalDate fecha, int id_cliente, int id_empleado, double subtotal, double iva,
+			double total) {
 		super();
 		this.id = id;
+		this.fecha = fecha;
 		this.id_cliente = id_cliente;
 		this.id_empleado = id_empleado;
 		this.subtotal = subtotal;
 		this.iva = iva;
 		this.total = total;
 	}
+
+
 	public int getId() {
 		return id;
 	}
+
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
+
+	public LocalDate getFecha() {
+		return fecha;
+	}
+
+
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+
+
 	public int getId_cliente() {
 		return id_cliente;
 	}
+
+
 	public void setId_cliente(int id_cliente) {
 		this.id_cliente = id_cliente;
 	}
+
+
 	public int getId_empleado() {
 		return id_empleado;
 	}
+
+
 	public void setId_empleado(int id_empleado) {
 		this.id_empleado = id_empleado;
 	}
+
+
 	public double getSubtotal() {
 		return subtotal;
 	}
+
+
 	public void setSubtotal(double subtotal) {
 		this.subtotal = subtotal;
 	}
+
+
 	public double getIva() {
 		return iva;
 	}
+
+
 	public void setIva(double iva) {
 		this.iva = iva;
 	}
+
+
 	public double getTotal() {
 		return total;
 	}
+
+
 	public void setTotal(double total) {
 		this.total = total;
 	}
+
+
 	@Override
 	public String toString() {
-		return "Factura [id=" + id + ", id_cliente=" + id_cliente + ", id_empleado=" + id_empleado + ", subtotal="
-				+ subtotal + ", iva=" + iva + ", total=" + total + "]";
+		return "Factura [id=" + id + ", fecha=" + fecha + ", id_cliente=" + id_cliente + ", id_empleado=" + id_empleado
+				+ ", subtotal=" + subtotal + ", iva=" + iva + ", total=" + total + "]";
 	}
 	
 	
--- Proyecto/src/dao/FacturaDAO.java
+++ Proyecto/src/dao/FacturaDAO.java
@@ -1,5 +1,65 @@
 package dao;
 
-public class FacturaDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import clase.Factura;
+import util.ConexionBD;
 
+public class FacturaDAO implements GenericDAO<Factura> {
+
+	@Override
+	public boolean insertar(Factura objeto) {
+		String sql = "insert into factura ( fecha, id_cliente, id_empleado, subtotal, iva, total) values (,?,?,?,?,?,?)";
+	    try (Connection con = ConexionBD.getConnection();
+	         PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
+	          ps.setObject(1, objeto.getFecha());
+	          ps.setInt(2, objeto.getId_cliente());
+	          ps.setInt(3, objeto.getId_empleado());
+	          ps.setDouble(4, objeto.getSubtotal());
+	          ps.setDouble(5, objeto.getIva());
+	          ps.setDouble(6, objeto.getTotal());
+	          
+	          int filas = ps.executeUpdate();
+	          if (filas > 0) {
+	                ResultSet rs = ps.getGeneratedKeys();
+	                if (rs.next()) {
+	                    objeto.setId(rs.getInt(1));
+	                }
+	                return true;
+	            }
+	      } catch (SQLException e) {
+	            System.out.println("Error al insertar: " + e.getMessage());
+	      }
+	        return false;
+	    }
+
+	@Override
+	public List<Factura> obtenerTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Factura obtenerPorId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean actualizar(Factura objeto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean eliminar(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
 }
